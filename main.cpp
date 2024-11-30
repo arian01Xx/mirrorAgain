9b647b30c4ecff2b33a9c6e42dc02713c4e4341e
Arian Joseph <arian_joseph@outlook.es>
2024-11-29 23:26:21 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class SolutionFour{
+public:
+    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+        vector<vector<int>> graph(n+1);
+        vector<bool> vis(n+1,false);
+        for(int i=0; i<edges.size(); i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        graph[1].push_back(0);
+        queue<pair<int,double>> q;
+        q.push({1,1.00});
+        vis[1]=true;
+        int level=0;
+
+        while(!q.empty()){
+            int len=q.size();
+            while(len--){
+                pair<int,double> currentNode=q.front();
+                q.pop();
+                int currentNodeIdx=currentNode.first;
+                double currentNodeProb=currentNode.second;
+                if(currentNodeIdx==target){
+                    if(graph[currentNodeIdx].size()==1 && t>=level){
+                        return currentNodeProb;
+                    }else if(level==t) return currentNodeProb;
+                    else return 0.00;
+                }
+                int s=graph[currentNodeIdx].size()-1;
+                for(int i=0; i<graph[currentNodeIdx].size(); i++){
+                    int child=graph[currentNodeIdx][i];
+                    double prob=(double)currentNodeProb/s;
+                    if(!vis[child] && child>0){
+                        q.push({child,prob});
+                        vis[child]=true;
+                    }
+                }
+            }
+            level++;
+        }
+        return 0.00;
+    }
+};
+
+int main(){
+    return 0;
+}
